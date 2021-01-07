6750a06c4eb321993f36b187786cc65680b61e70
Stanislaw Baranski <stachu@stasbar.com>
2021-01-07 13:51:38 +0100

    wip: multithreading

--- generator-nodejs/src/worker/index.ts
+++ generator-nodejs/src/worker/index.ts
@@ -1,3 +1,5 @@
-import { Worker, parentPort, workerData } from 'worker_threads';
+import WorkerPool from "./worker_pool";
+
+const pool = new WorkerPool(os.cpus().length);
 
 const worker = new Worker('./sendTx.js')
