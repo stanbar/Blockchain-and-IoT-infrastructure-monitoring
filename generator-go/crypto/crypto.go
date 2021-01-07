43c1f4388a23070dd14855a00406b3570f356a23
Stanislaw Baranski <stachu@stasbar.com>
2021-01-07 02:17:29 +0100

    feat: work on generator-go

--- generator-go/crypto/crypto.go
+++ generator-go/crypto/crypto.go
@@ -2,16 +2,45 @@ package crypto
 
 import (
 	"crypto/ed25519"
+	"crypto/sha256"
+	"crypto/sha512"
 	"golang.org/x/crypto/curve25519"
 )
 
-func DeriveDHKey(privKey ed25519.PrivateKey, pubKey ed25519.PublicKey) []byte {
-	var priv, pub, secret [32]byte
+func DeriveDHKey(privKey ed25519.PrivateKey, pubKey ed25519.PublicKey) ([]byte, error) {
+	var pub [32]byte
 
-	copy(priv[:], privKey[:32])
 	copy(pub[:], pubKey[:32])
-	secret = [32]byte{}
 
-	curve25519.ScalarMult(&secret, &priv, &pub)
-	return secret[:]
+	// From https://cr.yp.to/ecdh.html
+	scalar := GetScalar(privKey)
+	secret, err := ScalarMult(scalar[:], pubKey)
+	if err != nil {
+		return nil, err
+	}
+	result := sha256.Sum256(secret[:])
+	return result[:], err
+}
+
+func GetScalar(privKey []byte) [32]byte {
+	var scalar [32]byte
+	// From https://cr.yp.to/ecdh.html
+	hash := CalculateHash(privKey)
+	BytesOperations(&hash)
+	copy(scalar[:], hash[:32])
+	return scalar
+}
+
+func CalculateHash(privKey ed25519.PrivateKey) [64]byte {
+	return sha512.Sum512(privKey[:32])
+}
+
+func BytesOperations(bytes *[64]byte) {
+	bytes[0] &= 0xf8
+	bytes[31] &= 0x3f
+	bytes[31] |= 0x40
+}
+
+func ScalarMult(scalar, point []byte) ([]byte, error) {
+	return curve25519.X25519(scalar, point)
 }
--- generator-go/crypto/crypto_test.go
+++ generator-go/crypto/crypto_test.go
@@ -8,23 +8,72 @@ import (
 	"testing"
 
 	"github.com/stellar/go/keypair"
+	"github.com/stellar/go/strkey"
 )
 
-func TestDeriveDHKey(t *testing.T) {
-	alice := keypair.MustRandom()
-	bob := keypair.MustRandom()
-
-	aPub, aPriv, err := ed25519.GenerateKey(nil)
-	bPub, bPriv, err := ed25519.GenerateKey(nil)
+func keys(kp *keypair.Full) (ed25519.PublicKey, ed25519.PrivateKey) {
+	rawSeed := strkey.MustDecode(strkey.VersionByteSeed, kp.Seed())
+	reader := bytes.NewReader(rawSeed)
+	pub, priv, err := ed25519.GenerateKey(reader)
 	if err != nil {
-		log.Fatalln(err)
+		panic(err)
 	}
+	return pub, priv
+}
+
+func TestDeriveDHKey(t *testing.T) {
+	alice, err := keypair.ParseFull("SACSBRUH43EU4YBHK2UT4WOQKPIE4HLZPQBOIES7ZOAVMVCW5ZQRGVUZ")
+	if err != nil {
+		t.Error(err)
+	}
+	bob, err := keypair.ParseFull("SAAQ3OASEUGQQX6CRAWJTPZFAM6W2YOYBRIRNIYRUQY3QGGLFN4XYVRF")
+	if err != nil {
+		t.Error(err)
+	}
+
+	aPub, aPriv := keys(alice)
+	bPub, bPriv := keys(bob)
+
 	log.Println("aPub: ", hex.EncodeToString(aPub))
 	log.Println("aPriv: ", hex.EncodeToString(aPriv))
 	log.Println("bPub: ", hex.EncodeToString(bPub))
 	log.Println("bPriv: ", hex.EncodeToString(bPriv))
-	derivedByA := DeriveDHKey(aPriv, bPub)
-	derivedByB := DeriveDHKey(bPriv, aPub)
+
+	hash := CalculateHash(bPriv)
+	if hex.EncodeToString(hash[:]) != "7d26df77f87437f12e910b4a440d02227d0c5f28bd328533fa139cb9fe1742f8383a8722db43cf18187b3a6ffdb23d1ace65a5d270cc4a40d1ee65d464e986ae" {
+		t.Error("Hashes does not mach")
+	}
+
+	BytesOperations(&hash)
+	if hex.EncodeToString(hash[:]) != "7826df77f87437f12e910b4a440d02227d0c5f28bd328533fa139cb9fe174278383a8722db43cf18187b3a6ffdb23d1ace65a5d270cc4a40d1ee65d464e986ae" {
+		t.Error("Bytes operations failed")
+	}
+
+	scalar := hash[:32]
+	log.Println("scalar", hex.EncodeToString(scalar[:]))
+	// scalar: '7826df77f87437f12e910b4a440d02227d0c5f28bd328533fa139cb9fe174278'
+	if hex.EncodeToString(hash[:32]) != "7826df77f87437f12e910b4a440d02227d0c5f28bd328533fa139cb9fe174278" {
+		t.Error("Get Scalar does not match")
+	}
+
+	secret, err := ScalarMult(hash[:32], aPub)
+	if err != nil {
+		t.Error("Error", err)
+	}
+
+	log.Println("secret", hex.EncodeToString(secret[:]))
+	if hex.EncodeToString(secret[:]) != "bbeafe8cf8912ea4c8e49d4c53344ce5caaf9f9fb4c2501971d78b9450b5bc92" {
+		t.Error("Multiplication failed")
+	}
+
+	derivedByA, err := DeriveDHKey(aPriv, bPub)
+	if err != nil {
+		t.Error(err)
+	}
+	derivedByB, err := DeriveDHKey(bPriv, aPub)
+	if err != nil {
+		t.Error(err)
+	}
 	derivedByAString := hex.EncodeToString(derivedByA)
 	derivedByBString := hex.EncodeToString(derivedByB)
 	log.Println("Derived by A: ", derivedByAString)
