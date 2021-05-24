89e49a2550e5819fe6810fa561fc2e8b10c9ddcd
Stanislaw Baranski <stan@stan.bar>
2021-05-25 01:49:23 +0200

    feat: fix smart contract issues

--- fabric-smart-contract/log-event.go
+++ fabric-smart-contract/log-event.go
@@ -28,137 +28,130 @@ peer chaincode invoke -C myc1 -n log_event -c '{"Args":["GetFourth", "device-id"
 
 */
 
-
 package main
+
 import (
-  "encoding/json"
-  "fmt"
-  "log"
-  "time"
-  "github.com/golang/protobuf/ptypes"
-  "github.com/hyperledger/fabric-chaincode-go/shim"
-  "github.com/hyperledger/fabric-contract-api-go/contractapi"
+	"encoding/json"
+	"fmt"
+	"log"
+
+	"github.com/google/uuid"
+	"github.com/hyperledger/fabric-chaincode-go/shim"
+	"github.com/hyperledger/fabric-contract-api-go/contractapi"
 )
 
 type SimpleChaincode struct {
-  contractapi.Contract
+	contractapi.Contract
 }
 
 type RequestLog struct {
-  DocType        string `json:"docType"` //docType is used to distinguish the various types of objects in state database
-  DeviceId string `json:"deviceId"`
-  Value int `json:"value"`
-  Type string `json:"type"` // HUMD, TEMP
+	DocType         string `json:"docType"` //docType is used to distinguish the various types of objects in state database
+	DeviceId        string `json:"deviceId"`
+	Value           int    `json:"value"`
+	MeasurementUnit string `json:"measurementUnit"` // HUMD, TEMP
 }
 
 type ResponseLog struct {
-  ID string
-  DeviceId string `json:"deviceId"`
-  Value int `json:"value"`
-  Type string `json:"type"` // HUMD, TEMP
+	ID              string
+	DeviceId        string `json:"deviceId"`
+	Value           int    `json:"value"`
+	MeasurementUnit string `json:"measurementUnit"` // HUMD, TEMP
 }
 
 type Aggregation struct {
-  DeviceId string `json:"deviceId"`
-  TimeFrame string `json:"timeFrame"` // 5sec, 30sec, 1min, 30min, 1h, 4h, 12h, 1d
-  Avg int `json:"avg"`
-  Max int `json:"max"`
-  Min int `json:"min"`
+	DeviceId  string `json:"deviceId"`
+	TimeFrame string `json:"timeFrame"` // 5sec, 30sec, 1min, 30min, 1h, 4h, 12h, 1d
+	Avg       int    `json:"avg"`
+	Max       int    `json:"max"`
+	Min       int    `json:"min"`
 }
 
-
-
 func (t *SimpleChaincode) LogEvent(ctx contractapi.TransactionContextInterface, deviceId string, value int, measurementUnit string) error {
-  log := &Log {
-    DocType:   "log",
-    DeviceId: deviceId,
-    Value: valuem,
-    MeasurementUnit: measurementUnit,
-  }
-  assetBytes, err := json.Marshal(asset)
-  if err != nil {
-    return err
-  }
-  err = ctx.GetStub().PutState(assetID, assetBytes)
-  if err != nil {
-    return err
-  }
+	log := &RequestLog{
+		DocType:         "log",
+		DeviceId:        deviceId,
+		Value:           value,
+		MeasurementUnit: measurementUnit,
+	}
+	assetBytes, err := json.Marshal(log)
+	if err != nil {
+		return err
+	}
+	err = ctx.GetStub().PutState(uuid.New().String(), assetBytes)
+	if err != nil {
+		return err
+	}
+	return nil
 }
 
 func (t *SimpleChaincode) QueryLogs(ctx contractapi.TransactionContextInterface, queryString string) ([]*ResponseLog, error) {
-  return getQueryResultForQueryString(ctx, queryString)
+	return getQueryResultForQueryString(ctx, queryString)
 }
 
 func (t *SimpleChaincode) GetLogsFromDeviceId(ctx contractapi.TransactionContextInterface, deviceId string) ([]*ResponseLog, error) {
-  queryString := fmt.Sprintf(`{"selector":{"docType":"log","deviceId":"%s"}}`, deviceId)
-  return getQueryResultForQueryString(ctx, queryString)
+	queryString := fmt.Sprintf(`{"selector":{"docType":"log","deviceId":"%s"}}`, deviceId)
+	return getQueryResultForQueryString(ctx, queryString)
 }
-
-
 
 func getQueryResultForQueryString(ctx contractapi.TransactionContextInterface, queryString string) ([]*ResponseLog, error) {
-  resultsIterator, err := ctx.GetStub().GetQueryResult(queryString)
-  if err != nil { 
-    return nil, err
-  }
-  defer resultsIterator.Close()
-  return constructQueryResponseFromIterator(resultsIterator)
+	resultsIterator, err := ctx.GetStub().GetQueryResult(queryString)
+	if err != nil {
+		return nil, err
+	}
+	defer resultsIterator.Close()
+	return constructQueryResponseFromIterator(resultsIterator)
 }
-
 
 // constructQueryResponseFromIterator constructs a slice of assets from the resultsIterator
 func constructQueryResponseFromIterator(resultsIterator shim.StateQueryIteratorInterface) ([]*ResponseLog, error) {
-  var logs []*ResponseLog
-  for resultsIterator.HasNext() {
+	var logs []*ResponseLog
+	for resultsIterator.HasNext() {
 
-    queryResult, err := resultsIterator.Next()
-    if err != nil {
-      return nil, err
-    }
-    var log Log
-    err = json.Unmarshal(queryResult.Value, &log)
+		queryResult, err := resultsIterator.Next()
+		if err != nil {
+			return nil, err
+		}
+		var log ResponseLog
+		err = json.Unmarshal(queryResult.Value, &log)
 
-    if err != nil {
+		if err != nil {
 
-      return nil, err
-    }
-    logs = append(logs, &log)
+			return nil, err
+		}
+		logs = append(logs, &log)
 
-  }
-  return logs, nil
+	}
+	return logs, nil
 
 }
-
 
 // InitLedger creates the initial set of assets in the ledger.
 func (t *SimpleChaincode) InitLedger(ctx contractapi.TransactionContextInterface) error {
-  logs := []RequestLog{
-    {DocType: "log", DeviceId: "asdf", Value: 600, Type: "HUMD"},
-    {DocType: "log", DeviceId: "asdf", Value: 610, Type: "HUMD"},
-    {DocType: "log", DeviceId: "asdf", Value: 620, Type: "HUMD"},
-    {DocType: "log", DeviceId: "asdf", Value: 630, Type: "HUMD"},
-  }
+	logs := []RequestLog{
+		{DocType: "log", DeviceId: "asdf", Value: 600, MeasurementUnit: "HUMD"},
+		{DocType: "log", DeviceId: "asdf", Value: 610, MeasurementUnit: "HUMD"},
+		{DocType: "log", DeviceId: "asdf", Value: 620, MeasurementUnit: "HUMD"},
+		{DocType: "log", DeviceId: "asdf", Value: 630, MeasurementUnit: "HUMD"},
+	}
 
-  for _, log := range logs {
-    err := t.LogEvent(ctx, log.DeviceId, log.Value, log.Type)
-    if err != nil {
-      return err 
-    }
-  }
+	for _, log := range logs {
+		err := t.LogEvent(ctx, log.DeviceId, log.Value, log.MeasurementUnit)
+		if err != nil {
+			return err
+		}
+	}
 
-  return nil
+	return nil
 }
-
 
 func main() {
-  chaincode, err := contractapi.NewChaincode(&SimpleChaincode{})
+	chaincode, err := contractapi.NewChaincode(&SimpleChaincode{})
 
-  if err != nil {
-    log.Panicf("Error creating logs chaincode: %v", err)
-  }
+	if err != nil {
+		log.Panicf("Error creating logs chaincode: %v", err)
+	}
 
-  if err := chaincode.Start(); err != nil {
-    log.Panicf("Error starting logs chaincode: %v", err)
-  }
+	if err := chaincode.Start(); err != nil {
+		log.Panicf("Error starting logs chaincode: %v", err)
+	}
 }
-
