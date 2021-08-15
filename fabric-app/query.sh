89bb9f79182186ff9268f28e66008586c4c7650e
Stanislaw Baranski <stan@stan.bar>
2021-08-15 16:24:13 +0200

    docs: update

--- fabric-app/query.sh
+++ fabric-app/query.sh
@@ -1,1 +1,3 @@
-peer chaincode query -C mychannel -n logs5 -c '{"Args":["GetHistoryForKey", "asdf"]}' | jq
+export FABRIC_CFG_PATH=/home/stanbar/go/src/github.com/stanbar/fabric-samples/config/
+
+peer chaincode query -C mychannel -n stelliot -c '{"Args":["GetHistoryForKey", "1"]}' | jq
